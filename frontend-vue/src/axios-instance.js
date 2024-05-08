b7d5f7c01f2531a81f563c8d3c9d8b8d434019a1
DuhaKahya <duhakahyaa@gmail.com>
2024-05-08 15:38:23 +0200

    werkt

--- frontend-vue/src/axios-instance.js
+++ frontend-vue/src/axios-instance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/' // Adjust the baseURL to include the correct port and path
+    baseURL: 'http://localhost:8080/' // Adjust the baseURL to include the correct port and path
 });
 
 export default instance;
