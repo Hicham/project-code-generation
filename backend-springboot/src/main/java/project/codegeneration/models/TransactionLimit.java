fe1070e9b611922f7079742d20c0e8992fe0715c
Wouter023 <wouter2001@icloud.com>
2024-06-05 15:04:20 +0200

    FIX

--- backend-springboot/src/main/java/project/codegeneration/models/TransactionLimit.java
+++ backend-springboot/src/main/java/project/codegeneration/models/TransactionLimit.java
@@ -1,5 +1,6 @@
 package project.codegeneration.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ public class TransactionLimit {
     @Id
     private String IBAN;
 
+    @JsonBackReference
     @OneToOne(fetch = FetchType.LAZY)
     private Account account;
 
