9706f3444c0e8fbb98ecb4db5c25f19e5722cd5f
Hicham <hichamelans555@gmail.com>
2024-06-07 15:17:04 +0200

    Update vite.config.js

--- frontend-vue/vite.config.js
+++ frontend-vue/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project-code-generatie/',
+  base: '/project-code-generation/',
   plugins: [
     vue(),
   ],
